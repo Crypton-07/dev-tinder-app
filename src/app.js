a045117a3c7176da38b9c27574d7a11d6ac78ae1
vishesh <visheshprajapati786@gmail.com>
2025-01-11 16:00:52 +0530

    data validation and schema sanitization

--- src/app.js
+++ src/app.js
@@ -3,21 +3,90 @@ const app = express();
 const connectDB = require("./config/database");
 const User = require("./models/userSchema");
 
-app.post("/signup", async (req, res) => {
-  const userData = new User({
-    firstName: "Vishesh",
-    lastName: "Prajapati",
-    email: "visheh@gmail.com",
-    password: "3efdc4",
-    age: 25,
-    gender: "male",
-  });
+app.use(express.json());
 
+app.post("/signup", async (req, res) => {
+  // console.log(req.body);
+  const userData = new User(req.body);
   try {
     await userData.save();
     res.status(201).send("User data saved successfully");
   } catch (error) {
-    res.status(400).send("Failed to save user data");
+    res.status(400).send(error);
+  }
+});
+
+app.get("/user", async (req, res) => {
+  const user = await User.find({ email: req.body.email });
+  // console.log({ user });
+
+  try {
+    if (user) {
+      res.status(200).send(user);
+    } else {
+      res.status(404).send("User not found");
+    }
+  } catch (err) {
+    res.status(500).send("Internal server error");
+  }
+});
+
+app.get("/feed", async (req, res) => {
+  const allUsers = await User.find();
+  try {
+    if (allUsers.length) {
+      res.status(200).send(allUsers);
+    } else {
+      res.status(404).send("No users found");
+    }
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).send("Internal server error");
+  }
+});
+
+app.delete("/user", async (req, res) => {
+  const { userId } = req.body;
+  try {
+    if (userId) {
+      await User.findByIdAndDelete(userId);
+      res.status(200).send("User deleted successfully");
+    }
+  } catch (error) {
+    res.status(500).send("Internal server error");
+  }
+});
+
+app.patch("/user", async (req, res) => {
+  const data = req.body;
+  const ALLOWED_UPDATES = [
+    "firstName",
+    "lastName",
+    "age",
+    "skills",
+    "about",
+    "photoUrl",
+    "gender",
+  ];
+  try {
+    const isAllowedUpdates = Object.keys(data).every((k) =>
+      ALLOWED_UPDATES.includes(k)
+    );
+    const fieldIsNotAllowed = Object.keys(data).filter(
+      (k) => !ALLOWED_UPDATES.includes(k)
+    );
+    if (!isAllowedUpdates) {
+      throw new Error(
+        `Field ${fieldIsNotAllowed} is not allowed to update in user data`
+      );
+    }
+    await User.findOneAndUpdate({ email: data.email }, data, {
+      runValidators: true,
+    });
+    res.status(200).send("User data updated successfully");
+  } catch (error) {
+    res.status(500).send(error.message);
   }
 });
 
--- src/models/userSchema.js
+++ src/models/userSchema.js
@@ -1,14 +1,39 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
+const validator = require("validator");
 
-const userSchema = new Schema({
-  firstName: { type: String },
-  lastName: { type: String },
-  email: { type: String },
-  password: { type: String },
-  age: { type: Number },
-  gender: { type: String },
-});
+const userSchema = new Schema(
+  {
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Email is invalid - " + value);
+        }
+      },
+    },
+    password: { type: String, required: true },
+    age: { type: Number, min: 18 },
+    gender: {
+      type: String,
+      validate(value) {
+        if (!["male", "female", "other"].includes(value.toLowerCase())) {
+          throw new Error("Gender data is invalid");
+        }
+      },
+    },
+    photoUrl: { type: String },
+    about: { type: String, default: "Default description of user" },
+    skills: { type: [String] },
+  },
+  { timestamps: true }
+);
 
 const User = mongoose.model("User", userSchema);
 module.exports = User;
